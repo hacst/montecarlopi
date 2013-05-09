3b85d6f7f406e7366d1a3fbca8b1df0917d82635
Stefan Hacker <dd0t@users.sourceforge.net>
2013-05-10 01:22:11 +0200

    Initial import of visualized Monte Carlo Pi calculation in JavaScript

---
+++ montecarlopi.js
@@ -0,0 +1,66 @@
+﻿montecarlopi = function () {
+    (function () {
+        var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+                                    window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+        window.requestAnimationFrame = requestAnimationFrame;
+    })();
+
+    var hitsEl = document.getElementById('hits');
+    var missesEl = document.getElementById('misses');
+    var piEl = document.getElementById('pi');
+
+    var canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+
+    var width = canvas.width;
+    var height = canvas.height;
+    var radius = Math.min(width, height) / 2;
+
+    var hits = 0;
+    var misses = 0;
+    var total = 0;
+
+    // Draw circle
+    ctx.arc(width / 2, height / 2, radius, 0, 2 * Math.PI);
+    ctx.rect(0, 0, width, height);
+    ctx.stroke();
+
+    px = ctx.createImageData(1, 1);
+    px.data[0] = 255; // R
+    px.data[1] = 0; // G
+    px.data[2] = 0; // B
+    px.data[3] = 255; // A
+
+    function loop() {
+        for (var i = 0; i < 50; ++i) {
+            throwOne();
+        }
+        setTimeout(loop, 0);
+    }
+
+    function throwOne() {
+        // Throw a new one
+        var x = Math.random() * 2 - 1;
+        var y = Math.random() * 2 - 1;
+
+        if (Math.pow(x, 2) + Math.pow(y, 2) <= 1) {
+            ++hits;
+            hitsEl.textContent = hits
+        } else {
+            ++misses;
+            missesEl.textContent = misses;
+        }
+        ++total;
+
+        // Recalculate pi
+        piEl.textContent = 4 * hits / total;
+        
+        // Put pixel
+        var imgX = (x + 1) / 2 * width;
+        var imgY = (y + 1) / 2 * height;
+
+        ctx.putImageData(px, imgX, imgY);
+    };
+
+    setTimeout(loop, 0);
+};
